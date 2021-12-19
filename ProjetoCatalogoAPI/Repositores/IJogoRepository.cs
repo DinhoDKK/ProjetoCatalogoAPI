bb8a266e4af58645e216c66db5cf77c142c9ba37
Luiz Ricardo de Campos <87581372+DinhoDKK@users.noreply.github.com>
2021-12-19 18:07:44 -0400

    Adicionar arquivos de projeto.

---
+++ ProjetoCatalogoAPI/Repositores/IJogoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoCatalogoAPI.Entities;
+
+namespace ProjetoCatalogoAPI.Repositores
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid Id);
+        Task<Jogo> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid Id);
+    }
+}
